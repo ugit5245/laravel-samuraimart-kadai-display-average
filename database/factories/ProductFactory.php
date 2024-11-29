e03da818de87a309805418348dbe92fbf7b5d56b
ugit5245 <umex.orque2@gmail.com>
2024-11-29 22:05:29 +0900

    kadai33

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' =>fake()->name(),
+            'description'=>fake()->realtext(50),
+            'price' =>fake()->numberBetween(1,99999),
+            'category_id'=>1
+        ];
+    }
+}
